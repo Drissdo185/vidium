428faaf237ec910cda8bbba7612faef89e566a73
Đỗ Thành Đạt <dothanhdat185@gmail.com>
2025-01-21 23:00:18 +0700

    configuration

--- src/main/java/com/example/vidium/controller/StudentController.java
+++ src/main/java/com/example/vidium/controller/StudentController.java
@@ -1,4 +1,22 @@
 package com.example.vidium.controller;
 
+import com.example.vidium.model.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class StudentController {
+
+    private final List<Student> students = new ArrayList<>(List.of(
+            new Student(1, "Driss", 68),
+            new Student(2, "Kevin", 50)
+    ));
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        return students;
+    }
 }
--- src/main/java/com/example/vidium/model/Student.java
+++ src/main/java/com/example/vidium/model/Student.java
@@ -1,15 +1,31 @@
 package com.example.vidium.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
 import lombok.Getter;
+
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 @Getter
 @Setter
-@FieldDefaults(level= AccessLevel.PRIVATE)
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Student {
+    @JsonProperty("id")
     int id;
+
+    @JsonProperty("name")
     String name;
+
+    @JsonProperty("marks")
     int marks;
+
+    public Student() {
+    }
+
+    public Student(int id, String name, int marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
 }
